41a47eab851f39d4562f2c830e507ac374ffa458
Arpit <arpit.choudhary@timesinternet.in>
2020-05-30 16:24:14 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <p>Hello World</p>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello World</p>
+      </div>
+    )
+  }
 }
 
 export default App;
