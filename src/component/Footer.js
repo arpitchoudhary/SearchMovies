3dc3b4b8268bdd5877cb52787b8576a876ba8a6c
Arpit <arpit.choudhary@timesinternet.in>
2020-05-30 18:24:32 +0530

    linkedin profile attached to footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,8 +3,13 @@ import styles from './footer.module.css';
 
 export default function Footer() {
     return (
-        <div className= {styles.footer}>
+        <div className={styles.footer}>
             <p className={styles.p}>Developed By Arpit Choudhary</p>
+            <div class={styles.bottomright}>
+                <a href="https://www.linkedin.com/in/arpitchoudhary90/" target="_blank">
+                    <i class="fab fa-linkedin-in" id="linked-logo"></i>
+                </a>
+            </div>
         </div>
     )
 }
