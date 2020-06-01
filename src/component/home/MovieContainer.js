30d1d5ff27cb2d40cb83cc3279c8c302acc49708
Arpit <arpit.choudhary@timesinternet.in>
2020-06-02 00:21:22 +0530

    no search handling

--- src/component/home/MovieContainer.js
+++ src/component/home/MovieContainer.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
-
+import Error from '../layout/Error'
 import MovieCard from './MovieCard';
 
 export class MovieContainer extends Component {
   render() {
     const { movies } = this.props;
     let content = '';
+    let errorContent = '';
+
+    errorContent =
+      movies.Response === 'False' ? <Error></Error> : null;
 
     content =
       movies.Response === 'True'
         ? movies.Search.map((movie, index) => (
-            <MovieCard key={index} movie={movie} />
-          ))
+          <MovieCard key={index} movie={movie} />
+        ))
         : null;
-    return <div className="row">{content}</div>;
+
+    return <div className="row">{content}{errorContent}</div>;
   }
 }
 
---
+++ src/component/layout/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+    return (
+        <div>
+            <p>No Result found</p>
+        </div>
+    )
+}
