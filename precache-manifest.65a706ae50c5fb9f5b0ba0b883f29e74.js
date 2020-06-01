8b1e4ae661fca933b81c8b4dd40b118d54417227
Arpit <arpit.choudhary@timesinternet.in>
2020-06-02 00:36:11 +0530

    Updates

---
+++ precache-manifest.65a706ae50c5fb9f5b0ba0b883f29e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3284b00b892ac7097b23b68c894cdafd",
+    "url": "/SearchMovies/index.html"
+  },
+  {
+    "revision": "1269de926b4cf114e2b9",
+    "url": "/SearchMovies/static/css/main.4a8211c1.chunk.css"
+  },
+  {
+    "revision": "fe79c9e0a3747a1a9558",
+    "url": "/SearchMovies/static/js/2.ad058fec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SearchMovies/static/js/2.ad058fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1269de926b4cf114e2b9",
+    "url": "/SearchMovies/static/js/main.7a6d966d.chunk.js"
+  },
+  {
+    "revision": "c380b9945d37e270ab2a",
+    "url": "/SearchMovies/static/js/runtime-main.574b46f0.js"
+  },
+  {
+    "revision": "12c2e1df324ef94b5e692e3d088f2cb7",
+    "url": "/SearchMovies/static/media/loading.12c2e1df.gif"
+  }
+]);
